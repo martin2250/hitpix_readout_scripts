d8bfd180352bb9f931f589d8e47f4cae05e7db50
Martin Pittermann <martinp2250@gmail.com>
2022-03-06 23:42:08 +0100

    fastreadout reduce read size for better jitter

--- readout/fast_readout.py
+++ readout/fast_readout.py
@@ -44,7 +44,7 @@ class FastReadout:
         n_tot = 0
         t_start = time.perf_counter()
         while not self.event_stop.is_set():
-            data_new = self.ftdi.read(3968 * 8192) # see AN232B-03 Optimising D2XX Data Throughput
+            data_new = self.ftdi.read(3968 * 256) # see AN232B-03 Optimising D2XX Data Throughput
             if not isinstance(data_new, bytes) or not data_new:
                 time.sleep(0.001)
                 continue
