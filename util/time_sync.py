57d25db47bb3f24a469a49f8450ae66b2fd6c692
Martin Pittermann <martinp2250@gmail.com>
2022-04-04 14:41:15 +0200

    fix time synchronization

--- util/time_sync.py
+++ util/time_sync.py
@@ -1,18 +1,20 @@
 from dataclasses import dataclass, asdict
+from typing import TypeVar
 
 import numpy as np
 
+X = TypeVar('X', int, np.ndarray)
 
 @dataclass
 class TimeSync:
     sync_counter: int  # readout time register
     sync_time: float  # time.now()
-    tick_us: float
+    tick_us: float # actually in seconds
 
-    def convert(self, counter: int | np.ndarray) -> float | np.ndarray:
+    def convert(self, counter: X) -> X:
         # convert to unsigned
         counter_diff = (counter - self.sync_counter) & ((1 << 32) - 1)
-        return self.sync_time + (counter_diff * self.tick_us) * 1e-6
+        return (counter_diff * self.tick_us) + self.sync_time # type: ignore
 
     def asdict(self) -> dict:
         return asdict(self)
